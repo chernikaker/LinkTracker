01b5e70b465e0e73ae66fa5aff866bd09d69378f
Anna Shrubok <annashrubok21@gmail.com>
2025-02-19 12:46:18 +0300

    config: add lombok version

--- bot/src/main/java/backend/academy/bot/BotApplication.java
+++ bot/src/main/java/backend/academy/bot/BotApplication.java
@@ -1,8 +1,11 @@
 package backend.academy.bot;
 
+import backend.academy.bot.config.BotConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableConfigurationProperties({BotConfig.class})
