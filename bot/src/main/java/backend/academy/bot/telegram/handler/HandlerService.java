92c9daa4b92b2bab987dda637bb45513f0f37c4e
Anna Shrubok <annashrubok21@gmail.com>
2025-02-21 21:29:08 +0300

    feat(bot): add handler service for tg api update processings

---
+++ bot/src/main/java/backend/academy/bot/telegram/handler/HandlerService.java
@@ -0,0 +1,28 @@
+package backend.academy.bot.telegram.handler;
+
+import backend.academy.bot.model.Command;
+import backend.academy.bot.telegram.handler.state.CommandHandlerFactory;
+import backend.academy.bot.telegram.handler.state.CommandHandler;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import java.util.Optional;
+
+public class HandlerService {
+
+    private final CommandHandlerFactory commandHandlerFactory;
+
+    public HandlerService(
+        CommandHandlerFactory commandHandlerFactory) {
+        this.commandHandlerFactory = commandHandlerFactory;
+    }
+
+    public Optional<SendMessage> handle(Update update) {
+        if (update.message() == null || update.message().text() == null) {
+            return Optional.empty();
+        }
+        Command currentCommand = Command.fromString(update.message().text());
+        CommandHandler currentHandler = commandHandlerFactory.getHandlerByCommand(currentCommand);
+        SendMessage result = currentHandler.handleMessage(update.message());
+        return Optional.of(result);
+    }
+}
