1a55eaa55b7bb3d4ca85277364f9c134fdb99127
Anna Shrubok <annashrubok21@gmail.com>
2025-02-27 23:46:38 +0300

    fix(dto): change dto class to a record

--- dto/src/main/java/backend/academy/dto/AddLinkRequest.java
+++ dto/src/main/java/backend/academy/dto/AddLinkRequest.java
@@ -4,39 +4,17 @@ import backend.academy.data.Constant;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import java.util.List;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class AddLinkRequest {
-
-    /**
-     * URL для добавления.
-     * Не должен быть пустым и должен содержать не более 2048 символов.
-     */
+public record AddLinkRequest(
     @NotNull(message = "URL is required")
     @NotEmpty(message = "URL cannot be empty")
-    @Size(
-        max = Constant.MAX_URL_LENGTH,
-        message = "URL must be less than 2048 characters"
-    )
-    private String link;
+    @Size(max = Constant.MAX_URL_LENGTH, message = "URL must be less than 2048 characters")
+    String link,
 
-    /**
-     * Тэги для ссылки.
-     * Должны быть представлены в JSON, возможно пустым списком
-     */
-    @NotNull
-    private List<String> tags;
+    @NotNull(message = "Tags cannot be null")
+    List<String> tags,
 
-    /**
-     * Фильтры изменений.
-     * Должны быть представлены в JSON, возможно пустым списком
-     */
-    @NotNull
-    private List<String> filters;
-}
+    @NotNull(message = "Filters cannot be null")
+    List<String> filters
+) {}
