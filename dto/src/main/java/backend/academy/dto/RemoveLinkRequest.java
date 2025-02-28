5b9a27a7dea1d582bacfdf0aa95a69abb5fb3d23
Anna Shrubok <annashrubok21@gmail.com>
2025-02-28 10:44:43 +0300

    fix(dto): change dto from class to record

--- dto/src/main/java/backend/academy/dto/RemoveLinkRequest.java
+++ dto/src/main/java/backend/academy/dto/RemoveLinkRequest.java
@@ -4,24 +4,10 @@ import backend.academy.data.Constant;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class RemoveLinkRequest {
-
-    /**
-     * URL для удаления.
-     * Не должен быть пустым и должен содержать не более 2048 символов.
-     */
+public record RemoveLinkRequest(
     @NotNull(message = "URL is required")
     @NotEmpty(message = "URL cannot be empty")
-    @Size(
-        max = Constant.MAX_URL_LENGTH,
-        message = "URL must be less than 2048 characters"
-    )
-    private String link;
-}
+    @Size(max = Constant.MAX_URL_LENGTH, message = "URL must be less than 2048 characters")
+    String link
+) {}
