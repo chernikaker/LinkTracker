83b5eba926e51dbe8a31aef761c1e28e555ff2b6
Anna Shrubok <annashrubok21@gmail.com>
2025-03-01 18:02:48 +0300

    feat(bot-validator): add url validator

---
+++ bot/src/main/java/backend/academy/bot/validator/LinkUrlValidator.java
@@ -0,0 +1,16 @@
+package backend.academy.bot.validator;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.stereotype.Component;
+
+@UtilityClass
+public class LinkUrlValidator {
+
+    private static final String GITHUB_REPO_REGEX = "^https?:\\/\\/github\\.com\\/[a-zA-Z0-9_-]+\\/[a-zA-Z0-9_-]+$";
+    private static final String SO_QUESTION_REGEX = "^https?:\\/\\/stackoverflow\\.com\\/questions\\/\\d+(\\/[a-z0-9-]+)?$";
+
+    public boolean isValid(String link) {
+        return link.matches(SO_QUESTION_REGEX) || link.matches(GITHUB_REPO_REGEX);
+    }
+}
+
