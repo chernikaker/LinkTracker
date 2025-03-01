46cef048ef8e3d70ff060cf5890c57f0f1415fe9
Anna Shrubok <annashrubok21@gmail.com>
2025-03-01 17:42:04 +0300

    feat(bot-telegram): add setmyconnadns

--- bot/src/main/java/backend/academy/bot/telegram/TelegramBotService.java
+++ bot/src/main/java/backend/academy/bot/telegram/TelegramBotService.java
@@ -3,21 +3,25 @@ package backend.academy.bot.telegram;
 import backend.academy.bot.telegram.handler.HandlerService;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SetMyCommands;
+import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Optional;
 
 
 @Getter
+@Slf4j
 public class TelegramBotService extends TelegramBot {
 
-    private final Logger log = LoggerFactory.getLogger(TelegramBotService.class);
     private final HandlerService handlerService;
 
     public TelegramBotService(String telegramBotToken, HandlerService handlerService) {
@@ -27,12 +31,30 @@ public class TelegramBotService extends TelegramBot {
 
     @PostConstruct
     public void startBot() {
+        registerCommands();
         setUpdatesListener(list -> {
             for(Update update : list) {
                 handleUpdate(update);
             }
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
+    }
+
+    private void registerCommands() {
+        BotCommand[] commands = {
+            new BotCommand("/start", "Запуск бота и регистрация пользователя"),
+            new BotCommand("/help", "Помощь"),
+            new BotCommand("/track", "Добавить ссылку для отслеживания"),
+            new BotCommand("/untrack", "Прекратить отслеживание ссылки"),
+            new BotCommand("/list", "Список отслеживаемых ссылок")
+        };
+        SetMyCommands setMyCommands = new SetMyCommands(commands);
+        BaseResponse response = execute(setMyCommands);
+        if (response.isOk()) {
+            log.info("Set My Commands executed successfully");
+        } else {
+            log.error("Set My Commands failed");
+        }
     }
 
     private void handleUpdate(Update update) {
