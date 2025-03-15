226e5d30fa8e3c2269e293d13bc6f0e7a6fd20ef
Anna Shrubok <annashrubok21@gmail.com>
2025-03-15 09:55:47 +0300

    db: update initial schema

--- docker/migrations/00-initial-schema.sql
+++ docker/migrations/00-initial-schema.sql
@@ -1,50 +1,47 @@
-CREATE TABLE user(
+CREATE TABLE tg_user (
     id BIGSERIAL PRIMARY KEY,
-    chat_id BIGINT NOT NULL
-)
+    chat_id BIGINT NOT NULL UNIQUE
+);
 
-CREATE TYPE link_type AS ENUM ('GITHUB', 'STACKOVERFLOW');
-
-CREATE TABLE link(
+CREATE TABLE link (
     id BIGSERIAL PRIMARY KEY,
-    url VARCHAR(2048) NOT NULL,
-    type link_type NOT NULL,
+    url VARCHAR(2048) NOT NULL UNIQUE,
     last_validation TIMESTAMP
-)
+);
 
-CREATE TABLE subscription(
+CREATE TABLE subscription (
    id BIGSERIAL PRIMARY KEY,
    user_id BIGINT NOT NULL,
    link_id BIGINT NOT NULL,
-   FOREIGN KEY (user_id) REFERENCES User(id),
-   FOREIGN KEY (link_id) REFERENCES Link(id)
-)
+   FOREIGN KEY (user_id) REFERENCES tg_user(id),
+   FOREIGN KEY (link_id) REFERENCES link(id)
+);
 
-CREATE TABLE tag(
+CREATE TABLE tag (
     id BIGSERIAL PRIMARY KEY,
     tag_text VARCHAR(100) NOT NULL,
     user_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES User(id)
-)
+    FOREIGN KEY (user_id) REFERENCES tg_user(id)
+);
 
-CREATE TABLE filter(
+CREATE TABLE filter (
     id BIGSERIAL PRIMARY KEY,
     key VARCHAR(50) NOT NULL,
     value VARCHAR(50) NOT NULL
-)
+);
 
-CREATE TABLE subscription_tag(
+CREATE TABLE subscription_tag (
     subscription_id BIGINT NOT NULL,
     tag_id BIGINT NOT NULL,
     PRIMARY KEY (subscription_id, tag_id),
     FOREIGN KEY (subscription_id) REFERENCES subscription(id),
     FOREIGN KEY (tag_id) REFERENCES tag(id)
-)
+);
 
-CREATE TABLE subscription_tag(
+CREATE TABLE subscription_filter (
     subscription_id BIGINT NOT NULL,
-    filter_id BIGINT NOT NULL
-    PRIMARY KEY (subscription_id, filter_id)
+    filter_id BIGINT NOT NULL,
+    PRIMARY KEY (subscription_id, filter_id),
     FOREIGN KEY (subscription_id) REFERENCES subscription(id),
     FOREIGN KEY (filter_id) REFERENCES filter(id)
-)
+);
