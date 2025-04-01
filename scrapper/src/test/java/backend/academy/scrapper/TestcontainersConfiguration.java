4b5dd0f08ee987d49e94fc0c640f6df374c4fb4d
Anna Shrubok <annashrubok21@gmail.com>
2025-04-01 13:10:24 +0300

    test(scrapper): add testcontainers config

---
+++ scrapper/src/test/java/backend/academy/scrapper/TestcontainersConfiguration.java
@@ -0,0 +1,22 @@
+package backend.academy.scrapper;
+
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @RestartScope
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>("postgres:17-alpine")
+            .withExposedPorts(5432)
+            .withDatabaseName("local")
+            .withUsername("postgres")
+            .withPassword("test");
+    }
+}
