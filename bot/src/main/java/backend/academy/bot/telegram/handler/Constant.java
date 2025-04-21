eeac9d3faa1dcb9a5c1206deed9ef609870cd6f6
Anna Shrubok <annashrubok21@gmail.com>
2025-04-21 15:05:52 +0300

    refactor(bot): remove useless comments from constant

--- bot/src/main/java/backend/academy/bot/telegram/handler/Constant.java
+++ bot/src/main/java/backend/academy/bot/telegram/handler/Constant.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 /** Класс констант с сообщениями для пользователя */
 @UtilityClass
 public class Constant {
-    // common
+
     public static final String EMPTY_INPUT = "-";
     public static final String NOT_REGISTERED = "Вы не зарегистрированы. Чтобы зарегистрироваться, выполните /start";
     public static final String REQUEST_CANCELLED = "Запрос отклонен, попробуйте ещё раз";
@@ -17,14 +17,10 @@ public class Constant {
         Ссылка введена неверно или не поддерживается, попробуйте ещё раз.
         Подробнее о формате в /help
         """;
-
-    // FiltersTextCommandHandler
     public static final String LINK_REGISTERED = "\nСсылка успешно зарегистрирована!";
     public static final String LINK_UNABAILABLE = "Введенная ссылка недоступна, запрос отклонен";
     public static final String FILTERS_NOT_REGISTERD = "Фильтры не установлены";
     public static final String FILTERS_REGISTERED = "Фильтры установлены";
-
-    // HelpCommandHandler
     public static final String HELP_MESSAGE =
             """
         Бот поддерживает следующие ссылки и обновления:
@@ -47,25 +43,21 @@ public class Constant {
         - answer
         - comment
         """;
-
-    // LinkTextCommandHandler
     public static final String TAGS_TRACKING_MESSAGE = "Введите тэги(опционально, введите %s для пустых тегов)";
     public static final String NOT_EMPTY_TAGS_TRACKING_MESSAGE = "Введите тэги";
     public static final String TAG_TRACKING_MESSAGE = "Введите тэг (один)";
     public static final String TAG_INVALID_INPUT = "Введено несколько тэгов. Повторите попытку";
 
-    // ListCommandHandler
     public static final String NO_LINKS = "Отслеживаемых ссылок нет";
     public static final String LINK_HEADER = "Отслеживаемые ссылки:\n\n";
     public static final String TAG_HEADER = "Теги:\n";
     public static final String FILTER_HEADER = "Фильтры:\n";
     public static final String NO_TAGS = "Теги отсутствуют";
-    // StartCommandHandler
+
     public static final String CHAT_REGISTERED = "Чат успешно зарегистрирован";
     public static final String ALREADY_REGISTERED = "Вы уже зарегистрированы";
     public static final String REGISTRATION_CANCELLED = "Регистрация отклонена, попробуйте ещё раз";
 
-    // TagsTextCommandHandler
     public static final String ENTER_FILTER_NO_TAGS =
             "Тэги не установлены. Введите фильтры(опционально, введите %s для пустых фильтров)";
     public static final String ENTER_FILTER =
@@ -79,15 +71,11 @@ public class Constant {
     public static final String NO_TAG_FOR_USER = "Данный тег отсутствует. Сначала добавьте его";
     public static final String NO_TAG_FOR_SUBSCRIPTION = "Данный тег отсутствует. Сначала добавьте его к ссылке";
 
-    // TrackCommandHandler
     public static final String LINK_TRACK_MESSAGE = "Введите ссылку";
 
-    // UnknownCommandHandler
     public static final String UNKNOWN_COMMAND_MESSAGE = "Команда неизвестна или недоступна на данный момент";
 
-    // UntrackCommandHandler
     public static final String LINK_UNTRACK_TEXT = "Введите ссылку для удаления";
-
-    // UntrackingLinkTextCommandHandler
+    
     public static final String LINK_REMOVED_SUCCESS = "Ссылка успешно удалена";
 }
